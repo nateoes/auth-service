207a969a1fe1dda23bd58f9b3a3ff11f6968020b
mateo <lokirenegado@gmail.com>
2025-08-10 02:32:56 -0400

    ✨ Agregado endpoint /ping para verificación de servicio

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,11 @@ const router = express.Router();
 // Registro de usuario
 router.post('/register', registerUser);
 
+router.get('/ping', (req, res) => {
+  res.send('🏓 Pong desde Auth Service');
+});
+
+
 // Login de usuario
 router.post('/login', loginUser);
 
